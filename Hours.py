3e0eb72228d2713f57986d20b1c3c76ae7e0b70e
bryansandw <bryansandw@gmail.com>
2015-12-17 08:48:10 -0600

    Condensed code.

--- Hours.py
+++ Hours.py
@@ -8,24 +8,27 @@
 # ArcGIS Version: 10.2.2                                                    #
 # Python Version: 2.7                                                       #
 #############################################################################
-
 #Import arcpy module
 import arcpy
 
 # set workspace
 arcpy.env.overwriteOutput = True
-work_space = r'G:\GIS_PROJECTS\WATER_SERVICES\Rain_Gauges'
+work_space = 'G:\GIS_PROJECTS\WATER_SERVICES\Rain_Gauges'
 
 # input tables
-GolfCourse = r'./GolfCourse_Minute.txt' 
-Plant1 = r'./Plant 1_Minute.txt'
-Luza = r'./Luza 3_Minute.txt' 
-LiftStation = r'./LiftStation158_Minute.txt'
-Burgess = r'./Burgess LS_Minute.txt'
-LSPS = r'./LSPS Weather_Hour.txt'
+GolfCourse = './GolfCourse_Minute.txt' 
+Plant1 = './Plant 1_Minute.txt'
+Luza = './Luza 3_Minute.txt' 
+LiftStation = './LiftStation158_Minute.txt'
+Burgess = './Burgess LS_Minute.txt'
+LSPS = './LSPS Weather_Hour.txt'
+
+table_list = [GolfCourse, Plant1, Luza, LiftStation, Burgess]
+x_and_y = ['3545415,10218330', '3557931,10219830', '3546713,10227320', '3558420,10230450', '3538420,10203200']
+print_list = ["GolfCourse", "Plant1", "Luza", "LiftStation", "Burgess"]
 
 # output table
-out = r'./Hours_xy.txt'
+out = './Hours_xy.txt'
 out1 = open(out, 'w')
 
 # Create Headers in Output Text File
@@ -37,229 +40,62 @@ out1.write("Rain_in_Tot"+'\n')
 # Create point
 # Local variables:
 Hour_xy_Layer = "Minute_xy_Layer"
-shp = r'./Minute_xy.shp'
-Hour_xy = r'./Minute_xy.shp'
+shp = './Minute_xy.shp'
+Hour_xy = './Minute_xy.shp'
 
 #### I should try and get rid of these at some point
-Hour_xy__2_ = r'./Minute_xy.shp'
-Hour_xy__4_ = r'./Minute_xy.shp'
-Hour_xy__3_ = r'./Minute_xy.shp'
-Hour_xy_Layer_shp = r'./Minute_xy_Layer.shp'
+Hour_xy__2_ = './Minute_xy.shp'
+Hour_xy__4_ = './Minute_xy.shp'
+Hour_xy__3_ = './Minute_xy.shp'
+Hour_xy_Layer_shp = './Minute_xy_Layer.shp'
 
-#GolfCourse
-x = '3545415' 
-y = '10218330'  
-print 'Starting on Golf Course'
+# Variable for while loop
+i = 0
 
-with open(GolfCourse, "r") as f:
-    # read the lines and skip 4 line header
-    lines = f.readlines()[4:] 
+while i < len(table_list):
 
-    # need a local variable
-    last_date = ""
-    minute = 0
-    last_hour = 99
-    rain = 0.00
+    with open(table_list[i], "r") as f:
+    	# read the lines and skip 4 line header
+        lines = f.readlines()[4:] 
 
-    for line in lines:
-        items  = line.split(',') 
-        date, clock = items[0].split()
-        hour = clock.split(":")[0]
+		# need a local variable
+        last_date = ""
+        minute = 0
+        last_hour = 99
+        rain = 0.00
+    
+        for line in lines:
+            items  = line.split(',') 
+            date, clock = items[0].split()
+            hour = clock.split(":")[0]
 
-        if hour == last_hour:
-            rain_str = items[2]
-            rain += float(rain_str)
-            last_hour = hour
-            minute +=  1
-            if minute == 60:
-                #print "Date is: " + date
-                #print "Hour is: " + hour
-                #print "Rain is: " + str(rain)
-                minute = 0
-                rain_string = str(rain)
-                #print rain_string
-                out1.write( x + ',' + y + ',' + date + ' ' + hour + 
-                    ':00:00"' + ',' + rain_string + '\n')
+            if hour == last_hour:
+                rain_str = items[2]
+                rain += float(rain_str)
+                last_hour = hour
+                minute +=  1
+                if minute == 60:
+    				#print "Date is: " + date
+					#print "Hour is: " + hour
+					#print "Rain is: " + str(rain)
+                    minute = 0
+                    rain_string = str(rain)
+					#print rain_string
+                    out1.write( x_and_y[i] + ',' + date + ' ' + hour + 
+                        ':00:00"' + ',' + rain_string + '\n')
 
-        else:
-            minute = 1
-            last_hour = clock.split(":")[0]
-            rain_str = items[2]
-            rain = float(rain_str)
-f.close()
-
-#Plant1
-x = '3557931'
-y = '10219830'
-print 'Starting on Plant 1'
-
-with open(Plant1, "r") as f:
-    # read the lines and skip 4 line header
-    lines = f.readlines()[4:] 
-
-    # need a local variable
-    last_date = ""
-    minute = 0
-    last_hour = 99
-    rain = 0.00
-
-    for line in lines:
-        items  = line.split(',') 
-        date, clock = items[0].split()
-        hour = clock.split(":")[0]
-
-        if hour == last_hour:
-            rain_str = items[2]
-            rain += float(rain_str)
-            last_hour = hour
-            minute +=  1
-            if minute == 60:
-                #print "Date is: " + date
-                #print "Hour is: " + hour
-                #print "Rain is: " + str(rain)
-                minute = 0
-                rain_string = str(rain)
-                #print rain_string
-                out1.write( x + ',' + y + ',' + date + ' ' + hour + 
-                    ':00:00"' + ',' + rain_string + '\n')
-
-        else:
-            minute = 1
-            last_hour = clock.split(":")[0]
-            rain_str = items[2]
-            rain = float(rain_str)
-f.close()
-
-#Luza
-x = '3546713' 
-y = '10227320' 
-print 'Starting on Luza'
-
-with open(Luza, "r") as f:
-    # read the lines and skip 4 line header
-    lines = f.readlines()[4:] 
-
-    # need a local variable
-    last_date = ""
-    minute = 0
-    last_hour = 99
-    rain = 0.00
-
-    for line in lines:
-        items  = line.split(',') 
-        date, clock = items[0].split()
-        hour = clock.split(":")[0]
-
-        if hour == last_hour:
-            rain_str = items[2]
-            rain += float(rain_str)
-            last_hour = hour
-            minute +=  1
-            if minute == 60:
-                #print "Date is: " + date
-                #print "Hour is: " + hour
-                #print "Rain is: " + str(rain)
-                minute = 0
-                rain_string = str(rain)
-                #print rain_string
-                out1.write( x + ',' + y + ',' + date + ' ' + hour + 
-                    ':00:00"' + ',' + rain_string + '\n')
-
-        else:
-            minute = 1
-            last_hour = clock.split(":")[0]
-            rain_str = items[2]
-            rain = float(rain_str)
-f.close()
-
-#LiftStation 158
-x = '3558420' 
-y = '10230450' 
-print 'Starting on Lift Station'
-
-with open(LiftStation, "r") as f:
-    # read the lines and skip 4 line header
-    lines = f.readlines()[4:] 
-
-    # need a local variable
-    last_date = ""
-    minute = 0
-    last_hour = 99
-    rain = 0.00
-
-    for line in lines:
-        items  = line.split(',') 
-        date, clock = items[0].split()
-        hour = clock.split(":")[0]
-
-        if hour == last_hour:
-            rain_str = items[2]
-            rain += float(rain_str)
-            last_hour = hour
-            minute +=  1
-            if minute == 60:
-                #print "Date is: " + date
-                #print "Hour is: " + hour
-                #print "Rain is: " + str(rain)
-                minute = 0
-                rain_string = str(rain)
-                #print rain_string
-                out1.write( x + ',' + y + ',' + date + ' ' + hour + 
-                    ':00:00"' + ',' + rain_string + '\n')
-
-        else:
-            minute = 1
-            last_hour = clock.split(":")[0]
-            rain_str = items[2]
-            rain = float(rain_str)
-f.close()
-
-#Burgess
-x = '3538420' 
-y = '10203200' 
-print 'Starting on Burgess'
-
-with open(Burgess, "r") as f:
-    # read the lines and skip 4 line header
-    lines = f.readlines()[4:] 
-
-    # need a local variable
-    last_date = ""
-    minute = 0
-    last_hour = 99
-    rain = 0.00
-
-    for line in lines:
-        items  = line.split(',') 
-        date, clock = items[0].split()
-        hour = clock.split(":")[0]
-
-        if hour == last_hour:
-            rain_str = items[2]
-            rain += float(rain_str)
-            last_hour = hour
-            minute +=  1
-            if minute == 60:
-                #print "Date is: " + date
-                #print "Hour is: " + hour
-                #print "Rain is: " + str(rain)
-                minute = 0
-                rain_string = str(rain)
-                #print rain_string
-                out1.write( x + ',' + y + ',' + date + ' ' + hour + 
-                    ':00:00"' + ',' + rain_string + '\n')
-
-        else:
-            minute = 1
-            last_hour = clock.split(":")[0]
-            rain_str = items[2]
-            rain = float(rain_str)
-f.close()
+            else:
+                minute = 1
+                last_hour = clock.split(":")[0]
+                rain_str = items[2]
+                rain = float(rain_str)
+        print print_list[i]
+        i += 1
 
 #LSPS
 x = '3528380' 
 y = '10245400' 
-print 'Starting on LSPS'
+print 'Starting on LSPS' 
 
 with open(LSPS, "r") as f:
     # read the lines and skip 4 line header
@@ -269,6 +105,4 @@ with open(LSPS, "r") as f:
         item = line.split(',');
 #		rain = item[4].split('\n');
         out1.write( x + ',' + y + ',' + item[0] + ',' + item[14] )
-f.close()
-
 out1.close()
