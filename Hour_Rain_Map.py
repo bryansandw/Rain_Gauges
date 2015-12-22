1f93aa64efa0a4d9d6e0aaff5071f55d5d1ea8f5
bryansandw <bryansandw@gmail.com>
2015-12-22 12:40:19 -0600

    Update Hour_Rain_Map.py
    Maps export correctly. Improved the date selection input a little (there is still room for improvement).

--- Hour_Rain_Map.py
+++ Hour_Rain_Map.py
@@ -14,18 +14,18 @@ import datetime
 
 # set workspace
 arcpy.env.overwriteOutput = True
-work_space = 'G:\GIS_PROJECTS\WATER_SERVICES\Rain_Gauges'
-map_doc = work_space + '\Rain_Guage_Map.mxd'
+work_space = 'G:/GIS_PROJECTS/WATER_SERVICES/Rain_Gauges'
+map_doc = work_space + '/Rain_Guage_Map.mxd'
 
-hours_xy = work_space + '\Hour_xy.shp'
-hour_shp = work_space + '\hour.shp'
+hours_xy = work_space + '/Hour_xy.shp'
+hour_shp = work_space + '/hour.shp'
 
-mapdoc = arcpy.mapping.MapDocument(map_doc)
-data_frame = arcpy.mapping.ListDataFrames(mapdoc)[0]
+
+#data_frame = arcpy.mapping.ListDataFrames(my_mapdoc)[0]
 #print data_frame.name
 #print data_frame.scale
-legend = arcpy.mapping.ListLayoutElements(mapdoc, "LEGEND_ELEMENT", 
-        "Legend")[0] 
+#legend = arcpy.mapping.ListLayoutElements(my_mapdoc, "LEGEND_ELEMENT", 
+#        "Legend")[0] 
 
 hours = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24]
 dates = []
@@ -35,16 +35,8 @@ print "Do you wish to produce maps for all of the hours since Sep 24th, 2014?"
 print "Please answer Yes or No."
 
 answer1 = raw_input(">   ")
-
-if answer1 == "Yes" or answer1 == "yes":
-    print "This may take a while."
-    last_date = datetime.datetime.now() 
-    # could not get datetime.datetime(2014,09,24[,00[,00[,00[,00000[,None]]]]]) to work
-    d = datetime.date(2014,9,23)
-    t = datetime.time(00,0,0)
-    first_date = datetime.datetime.combine(d, t)
 	
-elif answer1 == "No" or answer1 == "no":
+if answer1 == "No" or answer1 == "no" or answer1 == "n":
     print """
 Great!
 You will be asked to input the first and last date in the range you are 
@@ -69,7 +61,7 @@ and 12-7-2015 for the last date.
         answer2 = raw_input(">   ")
         #items = answer2.split('-')
         f_mounth, f_day, f_year = answer2.split('-')
-        fd = datetime.date(f_year, f_mounth, f_day)
+        fd = datetime.date(int(f_year), int(f_mounth), int(f_day))
         ft = datetime.time(00,0,0)	
         first_date = datetime.datetime.combine(fd, ft)
     else:
@@ -77,11 +69,11 @@ and 12-7-2015 for the last date.
 
     print "Please input the last date in m-d-yyyy."
     answer4 = raw_input(">   ")
-    print answer2
-    #items = answer2.split('-')
+    print answer4
+    #items = answer4.split('-')
     l_mounth, l_day, l_year = answer4.split('-')
     ld = datetime.date(int(l_year), int(l_mounth), int(l_day))
-    lt = datetime.time(00,0,0)	
+    lt = datetime.time(00,0,0)
     last_date = datetime.datetime.combine(ld, lt)    
 
     print "Is %s-%s-%s the date you wanted for the last date?" % (l_mounth, l_day, l_year)
@@ -92,11 +84,19 @@ and 12-7-2015 for the last date.
         answer5 = raw_input(">   ")
         #items = answer2.split('-')
         l_mounth, l_day, l_year = answer5.split('-')
-        ld = datetime.date(l_year, l_mounth, l_day)
+        ld = datetime.date(int(l_year), int(l_mounth), int(l_day))
         lt = datetime.time(00,0,0)	
         last_date = datetime.datetime.combine(ld, lt)
     else:
         print "Great."
+
+else:
+    print "This may take a while."
+    last_date = datetime.datetime.now() 
+    # could not get datetime.datetime(2014,09,24[,00[,00[,00[,00000[,None]]]]]) to work
+    d = datetime.date(2014,9,23)
+    t = datetime.time(00,0,0)
+    first_date = datetime.datetime.combine(d, t)
 		
 		
 i = first_date
@@ -110,32 +110,30 @@ while i <= last_date :
 #s_cur = arcpy.da.SearchCursor(hours_xy, 
 #        ['Day', 'Hour', 'Rain_Total'])
 
-#Export to PDF not working
 for date in dates:
     for hour in hours:
         query = "\"Day\" = date'" + str(date) +"' AND \"Hour\" = " + str(hour)
         #print query
         #try:
         arcpy.Select_analysis( hours_xy, hour_shp, query)
-            #for text in arcpy.mapping.ListLayoutElements(mapdoc, "TEXT_ELEMENT"): 
-            #    if text.text == "Date: Text":
-            #        text.text = "Date: ", date
-            #    else:
-            #        pass
-            #    if text.text == "Hour: Text":
-            #        text.text = "Hour: ", hour
-            #    else:
-            #        pass
+        my_mapdoc = arcpy.mapping.MapDocument(map_doc)
+            
+        for text in arcpy.mapping.ListLayoutElements(my_mapdoc, "TEXT_ELEMENT"): 
+            if text.text == "Text":
+                text.text = "Date: " + str(date.date()) + '\n' + "Hour: " + str(hour)
+            else:
+                pass
         arcpy.RefreshActiveView() 
-        map_output = work_space + "\Rain_Maps\rain" + str(date) + "_" + str(hour) + ".pdf"
-        arcpy.mapping.ExportToPDF(mapdoc, map_output)
-        print "Created Map for ", date, hour
+        arcpy.RefreshTOC()
+            			
+        map_output = work_space + "/Rain_Maps/rain_" + str(date.date()) + "_" + str(hour) + ".pdf"
+        print map_output
+        arcpy.mapping.ExportToPDF(my_mapdoc, map_output)
+        del my_mapdoc
+            #print "Created Map for ", date, hour
             #print "selected" , hour, date
         #except:
-            #print "was unable to select", hour, date
-            #break #pass
-        
-   
+        #    print "Was unable to creat map for", hour, date
+        #    print "Make sure the Rain_Maps folder is closed."
+        #    break #pass
 
-
-arcpy.RefreshTOC() 
