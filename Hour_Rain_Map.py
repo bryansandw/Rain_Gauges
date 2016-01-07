121d902fb81a81184edc6ff35e8ebadea0e85675
bryansandw <bryansandw@gmail.com>
2016-01-07 12:41:28 -0600

    changed functions

--- Hour_Rain_Map.py
+++ Hour_Rain_Map.py
@@ -32,43 +32,33 @@ def assure_path_exists(my_path):
     if not os.path.exists(dir):
         os.mkdir(dir)
 
-def get_start_date(raw_user):
-    try:
-        f_mounth, f_day, f_year = raw_user.split('-')
-        fd = datetime.date(int(f_year), int(f_mounth), int(f_day))
-        ft = datetime.time(00,0,0)	
-        first_date = datetime.datetime.combine(fd, ft)
-        #print "Is %s-%s-%s the date you wanted for the first date?" % (f_mounth, f_day, f_year)
-        return (first_date)
-    except:
-        print "You input a bad date, we will use the first data record..."
-        d = datetime.date(2014,9,23)
+def get_date(raw_user):
+        month, day, year = raw_user.split('-')
+        d = datetime.date(int(year), int(month), int(day))
         t = datetime.time(00,0,0)
-        first_date = datetime.datetime.combine(d, t)
-        return (first_date)
+        date_ = datetime.datetime.combine(d, t)    
+        return (date_)	
 
-def get_end_date(raw_user):
-    try:
-        l_mounth, l_day, l_year = raw_user.split('-')
-        ld = datetime.date(int(l_year), int(l_mounth), int(l_day))
-        lt = datetime.time(00,0,0)
-        last_date = datetime.datetime.combine(ld, lt)    
-        #print "Is %s-%s-%s the date you wanted for the last date?" % (l_mounth, l_day, l_year)
-        return (last_date)	
-    except:
-        print "Today will do..."
-        last_date = datetime.datetime.now()     		
-        return (last_date)
 	
 # Get the user to input the dates to produce the desired maps 
 print "Please input the first date in m-d-yyyy."
-first_date = get_start_date(raw_input(">   "))
-print first_date
+try:
+    first_date = get_date(raw_input(">   "))
+    print first_date
+except:
+    print "You input a bad date, we will use the first data record..."
+    d = datetime.date(2014,9,23)
+    t = datetime.time(00,0,0)
+    first_date = datetime.datetime.combine(d, t)
 
 print "Please input the last date in m-d-yyyy."
-last_date = get_end_date(raw_input(">   "))
-print last_date
-
+try:
+    last_date = get_date(raw_input(">   "))
+    print last_date
+except:
+    print "Today will do..."
+    last_date = datetime.datetime.now()     		
+	
 # while loop fills in the dates list		
 i = first_date
 while i <= last_date :
@@ -92,24 +82,16 @@ for date in dates:
             for text in arcpy.mapping.ListLayoutElements(my_mapdoc, 
                     "TEXT_ELEMENT"): 
                 if text.text == "Text":
-                    text.text = "Date: " + str(date.date()) + '\n' 
-                        + "Hour: " + str(hour)
+                    text.text = "Date: " + str(date.date()) + '\n' + "Hour: " + str(hour)
+                         
                 else:
                     pass
             arcpy.RefreshActiveView() 
             arcpy.RefreshTOC()
             c_date = str(date.date())
-            map_output = work_space + "/Rain_Maps/Date" + c_date + "/rain_"
-                + c_date + "_" + str(hour) + ".gif"
+            map_output = work_space + "/Rain_Maps/Date" + c_date + "/rain_" + c_date + "_" + str(hour) + ".gif"
             assure_path_exists(map_output)
         
             print map_output
             arcpy.mapping.ExportToGIF(my_mapdoc, map_output)
             del my_mapdoc
-            #print "Created Map for ", date, hour
-            #print "selected" , hour, date
-        #except:
-        #    print "Was unable to create map for", hour, date
-        #    print "Make sure the Rain_Maps folder is closed."
-        #    break #pass
-
