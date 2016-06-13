46d94cfcc6edcb44c396b2b7097af08e36e125fb
bryansandw <bryansandw@gmail.com>
2016-06-13 15:59:24 -0500

    Update MINUTE4EXCEL.py

--- CSV2SHP/MINUTE4EXCEL.py
+++ CSV2SHP/MINUTE4EXCEL.py
@@ -9,7 +9,6 @@
 #############################################################################
 # Import module
 import datetime
-import os
 import numpy as np
 
 # set workspace
@@ -57,84 +56,55 @@ while i <= d_now:
 	i = new_date
 	
 # Create an array to store the values in the text files in
-my_array = np.zeros((len(dates) + 1,), dtype=[('Date', 'a'),
-	('GolfCourse(in)', '<u4'),
-	('Plant1(in)', '<u4'),
-	('Luza(in)', '<u4'),
-	('LiftStation(in)', '<u4'),
-	('Burgess(in)', '<u4'),
-	('LSPS(in)', '<u4'),
+my_array = np.zeros((len(dates),), dtype=[('Date', 'a', 21),
+	('GolfCourse', '<f'),
+	('Plant1', '<f'),
+	('Luza', '<f'),
+	('LiftStation', '<f'),
+	('Burgess', '<f'),
+	('LSPS', '<f'),
 ])
 
 #Go through and assign the dates and times to the first column of the array
-for date in dates:
-	my_array[dates.index(date)][0] = date
-	
-	
-
-print my_array
+my_array['Date'] = dates
+my_array['GolfCourse'] = -9999.9
+my_array['Plant1'] = -9999.9
+my_array['Luza'] = -9999.9
+my_array['LiftStation'] = -9999.9
+my_array['Burgess'] = -9999.9
+my_array['LSPS'] = -9999.9
+print my_array[2]	
+print my_array[200]
 
 #open the txt files and 
 
-for f, station in zip(table_list, index_list):
+for f, station in zip(table_list, print_list):
 	with open(f, "r") as f:
     # read the lines and skip 4 line header		
 		lines = f.readlines()[4:] 
 		for line in lines:
 			item = line.split(',')
 			try:
-				my_array[dates.index(item[0])][station] = item[2].rstrip('\n')
+				if station == 'LSPS':
+					my_array[dates.index(item[0])][station] = item[14].rstrip('\n')
+				else:
+					my_array[dates.index(item[0])][station] = item[2].rstrip('\n')
 			except:
-				print "Didin't work", f, station, item[0]
-	
-'''
-with open('./Minute_Rainfall_Events.txt', 'w') as outFile:
+				pass
+				#print "Didin't work", f, station, item[0]
 
-    # Create Headers in Output Text File
-    outFile.write('RAINFALL DATA\nRecorded from COB Rain Gauges\n' + str(d_first) + ' to ' + str(d_now) +'\nDate,GolfCourse(in),Plant1(in),Luza(in),LiftStation(in),Burgess(in),LSPS(in)\n')
-    for date in dates:
-		#print 'Starting on', date	
-		for f, station in zip(table_list, print_list):
-			with open(f, "r") as f:
-            # read the lines and skip 4 line header		
-				lines = f.readlines()[4:] 
-				for line in lines:
-					item = line.split(',')
-					if station == "GolfCourse":
-						if item[0] == date:
-							gc = item[2].rstrip('\n')
-							break
-						else:
-							gc = "N/A"
-					elif station == "Plant1":
-						if item[0] == date:
-							p1 = item[2].rstrip('\n')
-							break
-						else:
-							p1 = "N/A"
-					elif station == "Luza":
-						if item[0] == date:
-							lz = item[2].rstrip('\n')
-							break
-						else:
-							lz = "N/A"
-					elif station == "LiftStation":
-						if item[0] == date:
-							ls = item[2].rstrip('\n')
-							break
-						else:
-							ls = "N/A"
-					elif station == "Burgess":
-						if item[0] == date:
-							bg = item[2].rstrip('\n')
-							break
-						else:
-							bg = "N/A"
-					elif station == "LSPS":
-						if item[0] == date:
-							lsps = item[14].rstrip('\n')
-							break
-						else:
-							lsps = "N/A"			
-		outFile.write(str(date) + ',' + gc + ',' + p1 + ',' + lz + ',' + ls + ',' + bg + ',' + lsps + '\n')
-'''
+print my_array[2]	
+print my_array[200]				
+		
+with open('./Minute_Rainfall_Events.txt', 'w') as outFile:
+	outFile.write('RAINFALL DATA\nRecorded from COB Rain Gauges\n' + str(d_first) + ' to ' + str(d_now) +'\nDate,GolfCourse(in),Plant1(in),Luza(in),LiftStation(in),Burgess(in),LSPS(in)\n')
+	for date in dates:
+		day_time = my_array[dates.index(date)][0].strip("'")
+		golf = str(my_array[dates.index(date)][1])
+		plant = str(my_array[dates.index(date)][2])
+		luz = str(my_array[dates.index(date)][3])
+		lif_sta = str(my_array[dates.index(date)][4])
+		bur = str(my_array[dates.index(date)][5])
+		low_pump = str(my_array[dates.index(date)][6]) 
+		outFile.write(day_time + ', '+ golf + ', ' + plant + ', ' + luz + ', ' + lif_sta + ', ' + bur + ', ' + low_pump + '\n')
+
